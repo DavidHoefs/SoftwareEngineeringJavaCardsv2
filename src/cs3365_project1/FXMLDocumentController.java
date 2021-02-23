fadc1ca4e3db70f031674cb4f3007e83bdc89bee
David Hoefs <david.hoefs@ttu.edu>
2021-02-23 08:09:38 -0600

    First Commit

---
+++ src/cs3365_project1/FXMLDocumentController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs3365_project1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author hoefs
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Label label;
+    @FXML
+    private Button playBlackjackButton;
+    @FXML
+    private Button playPokerButton;
+    @FXML
+    private Button viewLeaderboardButton;
+    @FXML
+    private Label mainTitleLabel;
+    @FXML
+    private Button exitGameButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
